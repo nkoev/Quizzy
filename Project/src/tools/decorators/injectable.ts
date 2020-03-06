41abefd6337d6d3765638fc260f3e80b1c2ed80f
nkoev <koev.niki@gmail.com>
2020-03-06 02:02:18 +0200

    "tslint updated"

--- Project/src/tools/decorators/injectable.ts
+++ Project/src/tools/decorators/injectable.ts
@@ -1,5 +1,6 @@
 import { Class } from '../../types/class-type';
 
+// tslint:disable-next-line: variable-name
 export const Injectable = (): (target: Class<any>) => void =>
   (target: Class<any>) => {
     // For debugging and tracing injectables only
